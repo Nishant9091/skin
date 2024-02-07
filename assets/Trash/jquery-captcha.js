e04e7e48058aea3e1f8232d14259010b66a08405
Nishant <nishantpandat10053@gmail.com>
2024-02-07 16:49:32 +0530

    second

--- assets/Trash/jquery-captcha.js
+++ assets/Trash/jquery-captcha.js
@@ -1,51 +1,46 @@
-/*!
- * jquery-captcha-lgh v1.0 (https://github.com/honguangli/jquery-captcha-lgh)
- * Copyright honguangli
- * Licensed under the MIT license
- */
+
 ;
 let Captcha;
-(function($) {
+(function ($) {
   "use strict";
-  // 默认配置
   const defaults = {
-    element: null,       // canvas节点
-    length: 4,           // 校验码长度
-    code: [],            // 校验码
-    autoRefresh: false,   // 调用校验接口后是否自动刷新
+    element: null,
+    length: 4,
+    code: [],
+    autoRefresh: false,
   };
 
   const sCode = "A,B,C,E,F,G,H,J,K,L,M,N,P,Q,R,S,T,W,X,Y,Z,1,2,3,4,5,6,7,8,9,0";
   const aCode = sCode.split(",");
-  const aLength = aCode.length; //获取到数组的长度
+  const aLength = aCode.length;
 
-  Captcha = function(element, options) {
+  Captcha = function (element, options) {
     const self = this;
     self.options = $.extend(true, defaults, options);
     self.element = element;
     self.refresh();
     self.element.on('click', function () {
-        self.refresh();
+      self.refresh();
     });
   };
 
-  Captcha.prototype.refresh = function() {
+  Captcha.prototype.refresh = function () {
     const self = this;
     const canvas_width = self.element.width();
     const canvas_height = self.element.height();
-    const canvas = self.element[0]; //获取到canvas的对象，演员
-    const context = canvas.getContext("2d"); //获取到canvas画图的环境，演员表演的舞台
+    const canvas = self.element[0];
+    const context = canvas.getContext("2d");
     canvas.width = canvas_width;
     canvas.height = canvas_height;
 
     const code = [];
     for (let i = 0; i < self.options.length; i++) {
-      const j = Math.floor(Math.random() * aLength); //获取到随机的索引值
-      const deg = Math.random() * 30 * Math.PI / 180; //产生0~30之间的随机弧度
-      const txt = aCode[j]; //得到随机的一个内容
+      const j = Math.floor(Math.random() * aLength);
+      const deg = Math.random() * 30 * Math.PI / 180;
+      const txt = aCode[j];
       code.push(txt.toLowerCase());
-      const x = 10 + i * 20; //文字在canvas上的x坐标
-      const y = 20 + Math.random() * 8; //文字在canvas上的y坐标
+      const x = 10 + i * 20;
+      const y = 20 + Math.random() * 8;
       context.font = "bold 23px Arial";
 
       context.translate(x, y);
@@ -61,10 +56,8 @@ let Captcha;
     for (let i = 0; i <= 5; i++) {
       context.strokeStyle = randomColor();
       context.beginPath();
-      // 显示线条
       context.moveTo(Math.random() * canvas_width, Math.random() * canvas_height);
       context.lineTo(Math.random() * canvas_width, Math.random() * canvas_height);
-      // 显示小点
       const x = Math.random() * canvas_width;
       const y = Math.random() * canvas_height;
       context.moveTo(x, y);
@@ -72,7 +65,6 @@ let Captcha;
       context.stroke();
     }
 
-    //得到随机的颜色值
     function randomColor() {
       const r = Math.floor(Math.random() * 256);
       const g = Math.floor(Math.random() * 256);
@@ -81,11 +73,11 @@ let Captcha;
     }
   };
 
-  Captcha.prototype.getCode = function() {
+  Captcha.prototype.getCode = function () {
     return this.options.code.join('');
   };
 
-  Captcha.prototype.valid = function(code) {
+  Captcha.prototype.valid = function (code) {
     const self = this;
     const ans = code.toString().toLowerCase() === self.getCode().toLowerCase();
     if (!ans && self.options.autoRefresh) {
@@ -104,7 +96,7 @@ let Captcha;
 
 
 let CaptchaSecond;
-(function($) {
+(function ($) {
   "use strict";
   // 默认配置
   const defaults = {
@@ -118,17 +110,17 @@ let CaptchaSecond;
   const aCode = sCode.split(",");
   const aLength = aCode.length; //获取到数组的长度
 
-  CaptchaSecond = function(element, options) {
+  CaptchaSecond = function (element, options) {
     const self = this;
     self.options = $.extend(true, defaults, options);
     self.element = element;
     self.refresh();
     self.element.on('click', function () {
-        self.refresh();
+      self.refresh();
     });
   };
 
-  CaptchaSecond.prototype.refresh = function() {
+  CaptchaSecond.prototype.refresh = function () {
     const self = this;
     const canvas_width = self.element.width();
     const canvas_height = self.element.height();
@@ -180,11 +172,11 @@ let CaptchaSecond;
     }
   };
 
-  CaptchaSecond.prototype.getCode = function() {
+  CaptchaSecond.prototype.getCode = function () {
     return this.options.code.join('');
   };
 
-  CaptchaSecond.prototype.valid = function(code) {
+  CaptchaSecond.prototype.valid = function (code) {
     const self = this;
     const ans = code.toString().toLowerCase() === self.getCode().toLowerCase();
     if (!ans && self.options.autoRefresh) {
--- assets/Trash/script.js
+++ assets/Trash/script.js
@@ -1,48 +1,48 @@
-(function(){
+(function () {
     const envid = getParameterByName('eid', document.currentScript.src);
     let page_url = window.location.href;
-    
+
     //Anchor Click
     var link = document.getElementsByTagName("a");
-    for(var i=0;i<link.length;i++){
-        link[i].addEventListener('click', function(e) {
+    for (var i = 0; i < link.length; i++) {
+        link[i].addEventListener('click', function (e) {
             var data_arr = [];
             var link_url = this.href;
             var link_text = e.target.innerText;
-            data_arr.push({'page_url': page_url});
-            data_arr.push({'link_url': link_url});
-            data_arr.push({'link_text': link_text});
-            data_arr.push({'event': 'anchor'});
+            data_arr.push({ 'page_url': page_url });
+            data_arr.push({ 'link_url': link_url });
+            data_arr.push({ 'link_text': link_text });
+            data_arr.push({ 'event': 'anchor' });
             trackevent(data_arr);
         })
     }
 
     //Label Click
     var link = document.getElementsByTagName("label");
-    for(var i=0;i<link.length;i++){
-        link[i].addEventListener('click', function(e) {
+    for (var i = 0; i < link.length; i++) {
+        link[i].addEventListener('click', function (e) {
             var data_arr = [];
             var link_url = this.href;
             var link_text = e.target.innerText;
-            data_arr.push({'page_url': page_url});
-            data_arr.push({'link_url': link_url});
-            data_arr.push({'link_text': link_text});
-            data_arr.push({'event': 'label'});
+            data_arr.push({ 'page_url': page_url });
+            data_arr.push({ 'link_url': link_url });
+            data_arr.push({ 'link_text': link_text });
+            data_arr.push({ 'event': 'label' });
             trackevent(data_arr);
         })
     }
 
     //Button Click
     var link = document.getElementsByTagName("button");
-    for(var i=0;i<link.length;i++){
-        link[i].addEventListener('click', function(e) {
+    for (var i = 0; i < link.length; i++) {
+        link[i].addEventListener('click', function (e) {
             var data_arr = [];
             var link_url = this.href;
             var link_text = e.target.innerText;
-            data_arr.push({'page_url': page_url});
-            data_arr.push({'link_url': link_url});
-            data_arr.push({'link_text': link_text});
-            data_arr.push({'event': 'button'});
+            data_arr.push({ 'page_url': page_url });
+            data_arr.push({ 'link_url': link_url });
+            data_arr.push({ 'link_text': link_text });
+            data_arr.push({ 'event': 'button' });
             trackevent(data_arr);
         })
     }
@@ -51,9 +51,9 @@
         //Check Visitor
         if (getCookie("wbx_device_code") == "") {
             var data_arr = [];
-            data_arr.push({'link_url': page_url});
-            data_arr.push({'link_text': 'Page'});
-            data_arr.push({'event': 'visitor'});
+            data_arr.push({ 'link_url': page_url });
+            data_arr.push({ 'link_text': 'Page' });
+            data_arr.push({ 'event': 'visitor' });
             trackevent(data_arr);
         }
 
@@ -61,60 +61,60 @@
         document.querySelectorAll('form').forEach(e => e.addEventListener('submit', formsubmitted));
 
         //Page Scroll
-        document.addEventListener('scroll', function(){
+        document.addEventListener('scroll', function () {
             var h = document.documentElement, b = document.body, st = 'scrollTop', sh = 'scrollHeight';
-            var percent = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+            var percent = (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
             percent = percent.toFixed();
-            
+
             if (numberbetween(percent, 0, 25)) {
-                if(getCookie('scrolled') != 25){
+                if (getCookie('scrolled') != 25) {
                     setCookie("scrolled", percent, 1);
                     var data_arr = [];
-                    data_arr.push({'link_url': page_url});
-                    data_arr.push({'link_text': 'Scroll'});
-                    data_arr.push({'data': '0-25'});
-                    data_arr.push({'event': 'scroll'});
-                    data_arr.push({'percent': percent});
+                    data_arr.push({ 'link_url': page_url });
+                    data_arr.push({ 'link_text': 'Scroll' });
+                    data_arr.push({ 'data': '0-25' });
+                    data_arr.push({ 'event': 'scroll' });
+                    data_arr.push({ 'percent': percent });
                     trackevent(data_arr);
                 }
                 return 1;
 
             }
             if (numberbetween(percent, 26, 50)) {
-                if(getCookie('scrolled') != 50){
+                if (getCookie('scrolled') != 50) {
                     setCookie("scrolled", percent, 1);
                     var data_arr = [];
-                    data_arr.push({'link_url': page_url});
-                    data_arr.push({'link_text': 'Scroll'});
-                    data_arr.push({'data': '25-49'});
-                    data_arr.push({'event': 'scroll'});
-                    data_arr.push({'percent': percent});
+                    data_arr.push({ 'link_url': page_url });
+                    data_arr.push({ 'link_text': 'Scroll' });
+                    data_arr.push({ 'data': '25-49' });
+                    data_arr.push({ 'event': 'scroll' });
+                    data_arr.push({ 'percent': percent });
                     trackevent(data_arr);
                 }
                 return 1;
             }
             if (numberbetween(percent, 51, 75)) {
-                if(getCookie('scrolled') != 75){
+                if (getCookie('scrolled') != 75) {
                     setCookie("scrolled", percent, 1);
                     var data_arr = [];
-                    data_arr.push({'link_url': page_url});
-                    data_arr.push({'link_text': 'Scroll'});
-                    data_arr.push({'data': '50-74'});
-                    data_arr.push({'event': 'scroll'});
-                    data_arr.push({'percent': percent});
+                    data_arr.push({ 'link_url': page_url });
+                    data_arr.push({ 'link_text': 'Scroll' });
+                    data_arr.push({ 'data': '50-74' });
+                    data_arr.push({ 'event': 'scroll' });
+                    data_arr.push({ 'percent': percent });
                     trackevent(data_arr);
                 }
                 return 1;
             }
             if (numberbetween(percent, 76, 100)) {
-                if(getCookie('scrolled') != 100){
+                if (getCookie('scrolled') != 100) {
                     setCookie("scrolled", percent, 1);
                     var data_arr = [];
-                    data_arr.push({'link_url': page_url});
-                    data_arr.push({'link_text': 'Scroll'});
-                    data_arr.push({'data': '75-100'});
-                    data_arr.push({'event': 'scroll'});
-                    data_arr.push({'percent': percent});
+                    data_arr.push({ 'link_url': page_url });
+                    data_arr.push({ 'link_text': 'Scroll' });
+                    data_arr.push({ 'data': '75-100' });
+                    data_arr.push({ 'event': 'scroll' });
+                    data_arr.push({ 'percent': percent });
                     trackevent(data_arr);
                 }
                 return 1;
@@ -126,22 +126,22 @@
         return x == max;
     }
 
-    function formsubmitted(event){
+    function formsubmitted(event) {
         var data_arr = [];
-        data_arr.push({'link_url': page_url});
-        data_arr.push({'link_text': 'Form'});
-        data_arr.push({'data': 'Form'});
-        data_arr.push({'event': 'formsubmit'});
+        data_arr.push({ 'link_url': page_url });
+        data_arr.push({ 'link_text': 'Form' });
+        data_arr.push({ 'data': 'Form' });
+        data_arr.push({ 'event': 'formsubmit' });
         trackevent(data_arr);
     }
 
-    function trackevent(params){
+    function trackevent(params) {
         // Set Unique Visitor ID
         checkCookie();
-        
-        params.push({'config': envid});
+
+        params.push({ 'config': envid });
         let unique_device = getCookie("wbx_device_code");
-        params.push({'unique_device': unique_device});
+        params.push({ 'unique_device': unique_device });
 
         // let IpAddress = '';
         // fetch('https://ipv4.jsonip.com', { mode: 'cors'} )
@@ -157,12 +157,12 @@
         http.open('POST', url, true);
         http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
-        http.onreadystatechange = function() {
-            if(http.readyState == 4 && http.status == 200) {
+        http.onreadystatechange = function () {
+            if (http.readyState == 4 && http.status == 200) {
 
             }
         }
-        
+
         var dataJSON = JSON.stringify(params);
         http.send(dataJSON);
     }
@@ -178,36 +178,36 @@
     }
 
     //Unique Visitor
-    function setCookie(cname,cvalue,exdays) {
+    function setCookie(cname, cvalue, exdays) {
         const d = new Date();
-        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
         let expires = "expires=" + d.toUTCString();
         document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     }
-      
+
     function getCookie(cname) {
         let name = cname + "=";
         let decodedCookie = decodeURIComponent(document.cookie);
         let ca = decodedCookie.split(';');
-        for(let i = 0; i < ca.length; i++) {
-          let c = ca[i];
-          while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-          }
-          if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-          }
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
         }
         return "";
     }
-      
+
     function checkCookie() {
         let user = getCookie("wbx_device_code");
-        
-        if (user != "") { console.log('User exist: '+ user); } 
+
+        if (user != "") { console.log('User exist: ' + user); }
         else {
             user = uuidv4();
-            user = envid+"_"+user;
+            user = envid + "_" + user;
             // console.log(user);
             if (user != "" && user != null) {
                 setCookie("wbx_device_code", user, 30);
@@ -216,7 +216,7 @@
     }
 
     function uuidv4() {
-        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
             (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
         );
     }
